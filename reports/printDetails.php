a89f9ab8318a7ea441f969c0782b0e5292e410b2
joyceeeen <joycenpotestades@gmail.com>
2017-03-23 16:56:02 +0800

    ss

--- reports/printDetails.php
+++ reports/printDetails.php
@@ -1,7 +1,6 @@
 <?php
 require('fpdf/fpdf.php');
-mysql_connect('localhost','root','');
-mysql_select_db('blood_bank');
+$con = mysqli_connect('localhost','root','','blood_bank');
 $date = date("M.d, Y");
 $blood_types = "";
 session_start();
@@ -28,29 +27,29 @@ $pdf->SetFont('Arial','B',11);
 $pdf->Write(15,'                                                                                                                 Tayuman');
 $pdf->SetFont('Arial','B',11);
 $pdf->Ln(8);
-$Apositive = mysql_query("select * from blood where blood_type='1' ") or die(mysql_error());
-$numrowsOne = mysql_num_rows($Apositive);
+$Apositive = mysqli_query($con,"select * from blood where blood_type='1' ") or die(mysql_error());
+$numrowsOne = mysqli_num_rows($Apositive);
 
-$Anegative = mysql_query("select * from blood where blood_type='2' ") or die(mysql_error());
-$numrowsTwo = mysql_num_rows($Anegative);
+$Anegative = mysqli_query($con,"select * from blood where blood_type='2' ") or die(mysql_error());
+$numrowsTwo = mysqli_num_rows($Anegative);
 
-$Bpositive = mysql_query("select * from blood where blood_type='3' ") or die(mysql_error());
-$numrowsThree = mysql_num_rows($Bpositive);
+$Bpositive = mysqli_query($con,"select * from blood where blood_type='3' ") or die(mysql_error());
+$numrowsThree = mysqli_num_rows($Bpositive);
 
-$Bnegative = mysql_query("select * from blood where blood_type='4' ") or die(mysql_error());
-$numrowsFour = mysql_num_rows($Bnegative);
+$Bnegative = mysqli_query($con,"select * from blood where blood_type='4' ") or die(mysql_error());
+$numrowsFour = mysqli_num_rows($Bnegative);
 
-$Opositive = mysql_query("select * from blood where blood_type='7' ") or die(mysql_error());
-$numrowsFive = mysql_num_rows($Opositive);
+$Opositive = mysqli_query($con,"select * from blood where blood_type='7' ") or die(mysql_error());
+$numrowsFive = mysqli_num_rows($Opositive);
 
-$Onegative = mysql_query("select * from blood where blood_type='8' ") or die(mysql_error());
-$numrowsSix = mysql_num_rows($Onegative);
+$Onegative = mysqli_query($con,"select * from blood where blood_type='8' ") or die(mysql_error());
+$numrowsSix = mysqli_num_rows($Onegative);
 
-$ABpositive = mysql_query("select * from blood where blood_type='5' ") or die(mysql_error());
-$numrowsSeven = mysql_num_rows($ABpositive);
+$ABpositive = mysqli_query($con,"select * from blood where blood_type='5' ") or die(mysql_error());
+$numrowsSeven = mysqli_num_rows($ABpositive);
 
-$ABnegative = mysql_query("select * from blood where blood_type='6' ") or die(mysql_error());
-$numrowsEight = mysql_num_rows($ABnegative);
+$ABnegative = mysqli_query($con,"select * from blood where blood_type='6' ") or die(mysql_error());
+$numrowsEight = mysqli_num_rows($ABnegative);
 
 
 
@@ -75,64 +74,64 @@ $pdf->Ln(8);
 $pdf->Write(15,'                                                                                                                     for');
 $pdf->Write(10,'                                                                                                                                                                                                                                                   ' . $date);
 
-$pdf->SetFont('Arial', 'B', 12); 
+$pdf->SetFont('Arial', 'B', 12);
 $pdf->Ln(13);
 if($donorcategory == "All"){
     if($remarkscategory == 10 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood" ) or die(mysql_error());  }
     else if($remarkscategory == 10 && $blood_typescategory == 1){
-        $locquery=mysql_query("select * from blood where blood_type='1' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='1' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 2){
-        $locquery=mysql_query("select * from blood where blood_type='2' " ) or die(mysql_error()); }           
+        $locquery=mysqli_query($con,"select * from blood where blood_type='2' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 3){
-        $locquery=mysql_query("select * from blood where blood_type='3' " ) or die(mysql_error()); }           
+        $locquery=mysqli_query($con,"select * from blood where blood_type='3' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 4){
-        $locquery=mysql_query("select * from blood where blood_type='4' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='4' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 5){
-        $locquery=mysql_query("select * from blood where blood_type='5' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='5' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 6){
-        $locquery=mysql_query("select * from blood where blood_type='6' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='6' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 7){
-        $locquery=mysql_query("select * from blood where blood_type='7' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='7' " ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 8){
-        $locquery=mysql_query("select * from blood where blood_type='8' " ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='8' " ) or die(mysql_error()); }
     else if($remarkscategory == 0 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='0'" ) or die(mysql_error());  }            
+        $locquery=mysqli_query($con,"select * from blood where remarks='0'" ) or die(mysql_error());  }
     else if($remarkscategory == 1 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='1'" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood where remarks='1'" ) or die(mysql_error());  }
     else if($remarkscategory == 2 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='2'" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood where remarks='2'" ) or die(mysql_error());  }
     else{
-        $locquery=mysql_query("select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."'" ) or die(mysql_error());  }
+        $locquery=mysqli_query($con,"select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."'" ) or die(mysql_error());  }
 }
 else{
     if($remarkscategory == 10 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood WHERE donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood WHERE donor = '".$donorcategory."'" ) or die(mysql_error());  }
     else if($remarkscategory == 10 && $blood_typescategory == 1){
-        $locquery=mysql_query("select * from blood where blood_type='1' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='1' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 2){
-        $locquery=mysql_query("select * from blood where blood_type='2' and donor = '".$donorcategory."'" ) or die(mysql_error()); }           
+        $locquery=mysqli_query($con,"select * from blood where blood_type='2' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 3){
-        $locquery=mysql_query("select * from blood where blood_type='3' and donor = '".$donorcategory."'" ) or die(mysql_error()); }           
+        $locquery=mysqli_query($con,"select * from blood where blood_type='3' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 4){
-        $locquery=mysql_query("select * from blood where blood_type='4' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='4' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 5){
-        $locquery=mysql_query("select * from blood where blood_type='5' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='5' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 6){
-        $locquery=mysql_query("select * from blood where blood_type='6' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='6' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 7){
-        $locquery=mysql_query("select * from blood where blood_type='7' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='7' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 10 && $blood_typescategory == 8){
-        $locquery=mysql_query("select * from blood where blood_type='8' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+        $locquery=mysqli_query($con,"select * from blood where blood_type='8' and donor = '".$donorcategory."'" ) or die(mysql_error()); }
     else if($remarkscategory == 0 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='0' and donor = '".$donorcategory."'" ) or die(mysql_error());  }            
+        $locquery=mysqli_query($con,"select * from blood where remarks='0' and donor = '".$donorcategory."'" ) or die(mysql_error());  }
     else if($remarkscategory == 1 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='1' and donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood where remarks='1' and donor = '".$donorcategory."'" ) or die(mysql_error());  }
     else if($remarkscategory == 2 && $blood_typescategory == 10){
-        $locquery=mysql_query("select * from blood where remarks='2' and donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+        $locquery=mysqli_query($con,"select * from blood where remarks='2' and donor = '".$donorcategory."'" ) or die(mysql_error());  }
     else{
-        $locquery=mysql_query("select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."' and donor = '".$donorcategory."'" ) or die(mysql_error());  }
-}    
+        $locquery=mysqli_query($con,"select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."' and donor = '".$donorcategory."'" ) or die(mysql_error());  }
+}
 
 
 $pdf->Write(12,'               ');
@@ -143,13 +142,13 @@ $pdf->Write(12,'               ');
     $pdf->Cell(30, 6, 'Remarks', 1, 0, 'C');
     $pdf->Cell(30, 6, 'Nurse/Staff', 1, 0, 'C');
     $pdf->Cell(30, 6, 'Donor', 1, 0, 'C');
- 
+
 
 
         $pdf->Ln(6);
 
 $pdf->SetFont('Arial', '', 6);
-while($locrow=mysql_fetch_array($locquery))
+while($locrow=mysqli_fetch_array($locquery))
 {
 
     if($locrow['remarks'] == 0){
--- temp.php
+++ temp.php
@@ -1,72 +1,71 @@
-<?php 
-	mysql_connect('localhost','root','');
-	mysql_select_db('blood_bank');
+<?php
+	$con = mysqli_connect('localhost','root','','blood_bank');
 	$remarkscategory = $_POST['remarkscategory'];
 	$blood_typescategory = $_POST['blood_typescategory'];
 	$donorcategory = $_POST['donorcategory'];
 	$ifHas = false;
 	if($donorcategory == "All"){
 	    if($remarkscategory == 10 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 10 && $blood_typescategory == 1){
-	        $locquery=mysql_query("select * from blood where blood_type='1' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='1' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 2){
-	        $locquery=mysql_query("select * from blood where blood_type='2' " ) or die(mysql_error()); }           
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='2' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 3){
-	        $locquery=mysql_query("select * from blood where blood_type='3' " ) or die(mysql_error()); }           
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='3' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 4){
-	        $locquery=mysql_query("select * from blood where blood_type='4' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='4' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 5){
-	        $locquery=mysql_query("select * from blood where blood_type='5' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='5' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 6){
-	        $locquery=mysql_query("select * from blood where blood_type='6' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='6' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 7){
-	        $locquery=mysql_query("select * from blood where blood_type='7' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='7' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 8){
-	        $locquery=mysql_query("select * from blood where blood_type='8' " ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='8' " ) or die(mysqli_error()); }
 	    else if($remarkscategory == 0 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='0'" ) or die(mysql_error());  }            
+	        $locquery=mysqli_query($con,"select * from blood where remarks='0'" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 1 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='1'" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood where remarks='1'" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 2 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='2'" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood where remarks='2'" ) or die(mysqli_error());  }
 	    else{
-	        $locquery=mysql_query("select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."'" ) or die(mysql_error());  
+	        $locquery=mysqli_query($con,"select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."'" ) or die(mysqli_error());
 	    }
 
-	    if(mysql_num_rows($locquery) > 0){
+	    if(mysqli_num_rows($locquery) > 0){
 	    	$ifHas = true;
 	    }
 	}
 	else {
 	    if($remarkscategory == 10 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood WHERE donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood WHERE donor = '".$donorcategory."'" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 10 && $blood_typescategory == 1){
-	        $locquery=mysql_query("select * from blood where blood_type='1' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='1' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 2){
-	        $locquery=mysql_query("select * from blood where blood_type='2' and donor = '".$donorcategory."'" ) or die(mysql_error()); }           
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='2' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 3){
-	        $locquery=mysql_query("select * from blood where blood_type='3' and donor = '".$donorcategory."'" ) or die(mysql_error()); }           
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='3' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 4){
-	        $locquery=mysql_query("select * from blood where blood_type='4' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='4' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 5){
-	        $locquery=mysql_query("select * from blood where blood_type='5' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='5' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 6){
-	        $locquery=mysql_query("select * from blood where blood_type='6' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='6' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 7){
-	        $locquery=mysql_query("select * from blood where blood_type='7' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='7' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 10 && $blood_typescategory == 8){
-	        $locquery=mysql_query("select * from blood where blood_type='8' and donor = '".$donorcategory."'" ) or die(mysql_error()); }            
+	        $locquery=mysqli_query($con,"select * from blood where blood_type='8' and donor = '".$donorcategory."'" ) or die(mysqli_error()); }
 	    else if($remarkscategory == 0 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='0' and donor = '".$donorcategory."'" ) or die(mysql_error());  }            
+	        $locquery=mysqli_query($con,"select * from blood where remarks='0' and donor = '".$donorcategory."'" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 1 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='1' and donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood where remarks='1' and donor = '".$donorcategory."'" ) or die(mysqli_error());  }
 	    else if($remarkscategory == 2 && $blood_typescategory == 10){
-	        $locquery=mysql_query("select * from blood where remarks='2' and donor = '".$donorcategory."'" ) or die(mysql_error());  }          
+	        $locquery=mysqli_query($con,"select * from blood where remarks='2' and donor = '".$donorcategory."'" ) or die(mysqli_error());  }
 	    else{
-	        $locquery=mysql_query("select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."' and donor = '".$donorcategory."'" ) or die(mysql_error());  
+	        $locquery=mysqli_query($con,"select * from blood where remarks='".$remarkscategory."' and blood_type = '".$blood_typescategory."' and donor = '".$donorcategory."'" ) or die(mysqli_error());
 	    }
-	    if(mysql_num_rows($locquery) > 0){
+	    if(mysqli_num_rows($locquery) > 0){
 	    	$ifHas = true;
 	    }
 	}
@@ -79,7 +78,7 @@
 
  	}
 	else{
-		
+
 		$_SESSION['no_entry'] = 'No Results found!';
 		header("location:blood_inventory.php");
 	}
